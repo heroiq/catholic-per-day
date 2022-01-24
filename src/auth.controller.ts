aeae8c2cb32b5974131f7b758563728cb808a502
michael@mharris.io <19492392+Burwin@users.noreply.github.com>
2022-01-24 15:34:34 -0500

    implement callback

--- src/auth.controller.ts
+++ src/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller()
@@ -8,5 +8,18 @@ export class AuthController {
   @Get('authorize')
   async getAuthorize(): Promise<string> {
     return await this.auth.generateAuthLink();
+  }
+
+  @Get('twitter-callback')
+  async handleCallback(
+    @Query('state') state: string,
+    @Query('code') code: string,
+  ): Promise<string> {
+    try {
+      await this.auth.handleCallback(state, code);
+      return 'success';
+    } catch (error) {
+      return error;
+    }
   }
 }
--- src/auth.service.ts
+++ src/auth.service.ts
@@ -4,13 +4,15 @@ import { TwitterApi } from 'twitter-api-v2';
 import { AppConfig } from './app-config';
 import { baseUrl, clientId, clientSecret } from './config';
 
+const redirectUri = `${baseUrl}/twitter-callback`;
+
 @Injectable()
 export class AuthService {
   async generateAuthLink(): Promise<string> {
     const client = new TwitterApi({ clientId, clientSecret });
     const { url, codeVerifier, state } = client.generateOAuth2AuthLink(
-      `${baseUrl}/twitter-callback`,
-      { scope: ['tweet.read', 'tweet.write', 'users.read'] },
+      redirectUri,
+      { scope: ['tweet.read', 'tweet.write', 'users.read', 'offline.access'] },
     );
 
     // store codeVerifier and state for later
@@ -23,5 +25,37 @@ export class AuthService {
     await appConfigDoc.update(appConfig);
 
     return url;
+  }
+
+  async handleCallback(state: string, code: string): Promise<void> {
+    // get stored code and state
+    const firestore = new Firestore();
+    const appConfigDoc = firestore.collection('bots').doc('catholic-per-day');
+    const appConfig = (await appConfigDoc.get()).data() as AppConfig;
+
+    if (
+      !code ||
+      !state ||
+      !appConfig.auth.codeVerifier ||
+      !appConfig.auth.state
+    ) {
+      throw new Error('You denied the app or your session expired');
+    }
+
+    if (state !== appConfig.auth.state) {
+      throw new Error('Stored tokens do not match');
+    }
+
+    const twitter = new TwitterApi({ clientId, clientSecret });
+    const loginResult = await twitter.loginWithOAuth2({
+      code,
+      codeVerifier: appConfig.auth.codeVerifier,
+      redirectUri,
+    });
+
+    // store access token and refresh token
+    appConfig.auth.accessToken = loginResult.accessToken;
+    appConfig.auth.refreshToken = loginResult.refreshToken;
+    await appConfigDoc.update(appConfig);
   }
 }
