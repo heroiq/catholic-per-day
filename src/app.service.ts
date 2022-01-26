476f10e8e458146dd85b6e68bd19cf0181308d43
michael@mharris.io <19492392+Burwin@users.noreply.github.com>
2022-01-26 14:54:48 -0500

    implement tweeting endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { getCatholicDailyReadings } from 'get-catholic-daily-readings';
+import { buildTweets, TwitterAutoThreadClient } from 'twitter-auto-thread';
 
 @Injectable()
 export class AppService {
+  constructor(private auth: AuthService) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async tweetDailyReadings(): Promise<void> {
+    const twitterBase = await this.auth.refreshTwitterAccessToken();
+    const readings = await getCatholicDailyReadings();
+    const twitter = new TwitterAutoThreadClient(twitterBase);
+
+    const tweets = [
+      ...buildTweets(`${readings.header}\nLectionary: ${readings.lectionary}`),
+      ...readings.readings.flatMap((r) => {
+        return [...buildTweets(`${r.header}\n${r.reference}`), ...buildTweets(r.formattedText)];
+      }),
+    ];
+
+    await twitter.tweetThread(tweets);
   }
 }
